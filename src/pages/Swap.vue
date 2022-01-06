29dc6614240bce937135e3ae5adf28b00a5da94d
0xCoDefi <>
2022-01-06 21:44:45 +0000

    Initial CICD

--- src/pages/Swap.vue
+++ src/pages/Swap.vue
@@ -45,9 +45,14 @@
             :hidden="[assetInAddressInput, assetOutAddressInput]"
             @select="handleAssetSelect"
         />
-        <div class="text-secondary" style="font-size:11px">
+        <div
+            class="text-secondary"
+            style="font-size:11px"
+        >
             Current Network:<span>{{ network }} </span>
-        </div><br>
+            <br>
+            # {{ buildNumber }}
+        </div>
     </div>
 </template>
 
@@ -81,6 +86,8 @@ import SwapPair from '@/components/swap/Pair.vue';
 import ERC20ABI from '@/abi/ERC20.json';
 // eslint-disable-next-line no-undef
 const GAS_PRICE = process.env.APP_GAS_PRICE || '100000000000';
+// eslint-disable-next-line no-undef
+const BUILD_NUMBER = process.env.APP_BUILD_NUMBER || '001';
 const MAX_POOLS = 4;
 
 interface Pair {
@@ -126,7 +133,6 @@ export default defineComponent({
             }
             return address;
         });
-
         const validation = computed(() => {
             // Invalid input
             const inputError = validateNumberInput(activeInput.value);
@@ -772,6 +778,7 @@ export default defineComponent({
             unlock,
             swap,
             network,
+            buildNumber: BUILD_NUMBER,
         };
     },
 });
