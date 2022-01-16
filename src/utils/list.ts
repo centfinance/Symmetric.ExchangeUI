3204c117c4e65d3671e8e62412ef7fadc7b1cdc6
0xCoDefi <>
2022-01-16 16:51:33 +0000

    Tokenlist added to local

--- src/utils/list.ts
+++ src/utils/list.ts
@@ -1,4 +1,4 @@
-import tokenlist from '@centfinance/cent.dex_assets/generated/listed.tokenlist.json';
+import tokenlist from '../data/listed.tokenlist.json';
 
 import config, { AssetMetadata } from '@/config';
 
@@ -46,99 +46,99 @@ export function getAssetsFromTokenlist(chainId: number, list: TokenList): Record
     const assets: Record<string, AssetMetadata> = {};
     
     switch (config.network) {
-        case 'xdai':
-            if (list.tokens.findIndex(token => token.address === config.addresses.wxdai) !== -1) {
-                assets.xdai = {
-                    address: 'xdai',
-                    name: 'Xdai',
-                    symbol: 'XDAI',
-                    decimals: 18,
-                    logoURI: XDAI_LOGO,
-                };
+    case 'xdai':
+        if (list.tokens.findIndex(token => token.address === config.addresses.wxdai) !== -1) {
+            assets.xdai = {
+                address: 'xdai',
+                name: 'Xdai',
+                symbol: 'XDAI',
+                decimals: 18,
+                logoURI: XDAI_LOGO,
+            };
+        }
+        for (const token of list.tokens) {
+            if (token.chainId !== chainId) {
+                continue;
             }
-            for (const token of list.tokens) {
-                if (token.chainId !== chainId) {
-                    continue;
-                }
-                assets[token.address] = {
-                    address: token.address,
-                    name: token.name,
-                    symbol: token.symbol,
-                    decimals: token.decimals,
-                    logoURI: token.logoURI,
-                };
+            assets[token.address] = {
+                address: token.address,
+                name: token.name,
+                symbol: token.symbol,
+                decimals: token.decimals,
+                logoURI: token.logoURI,
+            };
+        }
+        break;
+    case 'sokol':
+        if (list.tokens.findIndex(token => token.address === config.addresses.wspoa) !== -1) {
+            assets.spoa = {
+                address: 'spoa',
+                name: 'Spoa',
+                symbol: 'SPOA',
+                decimals: 18,
+                logoURI: XDAI_LOGO,
+            };
+        }
+        for (const token of list.tokens) {
+            if (token.chainId !== chainId) {
+                continue;
             }
-            break;
-        case 'sokol':
-            if (list.tokens.findIndex(token => token.address === config.addresses.wspoa) !== -1) {
-                assets.spoa = {
-                    address: 'spoa',
-                    name: 'Spoa',
-                    symbol: 'SPOA',
-                    decimals: 18,
-                    logoURI: XDAI_LOGO,
-                };
+            assets[token.address] = {
+                address: token.address,
+                name: token.name,
+                symbol: token.symbol,
+                decimals: token.decimals,
+                logoURI: token.logoURI,
+            };
+        }
+        break;
+    case 'celo':
+    case 'alfajores':
+    case 'avalanche':
+    case 'fuji':
+    case 'fantom':
+    case 'fantom-testnet':
+    case 'optimism':
+    case 'optimism-kovan':
+    case 'polygon':
+    case 'polygon-mumbai':
+        for (const token of list.tokens) {
+            if (token.chainId !== chainId) {
+                continue;
             }
-            for (const token of list.tokens) {
-                if (token.chainId !== chainId) {
-                    continue;
-                }
-                assets[token.address] = {
-                    address: token.address,
-                    name: token.name,
-                    symbol: token.symbol,
-                    decimals: token.decimals,
-                    logoURI: token.logoURI,
-                };
+            assets[token.address] = {
+                address: token.address,
+                name: token.name,
+                symbol: token.symbol,
+                decimals: token.decimals,
+                logoURI: token.logoURI,
+            };
+        }
+        break;
+    case 'ethereum':
+    default:
+        if (list.tokens.findIndex(token => token.address === config.addresses.weth) !== -1) {
+            assets.ether = {
+                address: 'ether',
+                name: 'Ether',
+                symbol: 'ETH',
+                decimals: 18,
+                logoURI: ETH_LOGO,
+            };
+        }
+        for (const token of list.tokens) {
+            if (token.chainId !== chainId) {
+                continue;
             }
-            break;
-        case 'celo':
-        case 'alfajores':
-        case 'avalanche':
-        case 'fuji':
-        case 'fantom':
-        case 'fantom-testnet':
-        case 'optimism':
-        case 'optimism-kovan':
-        case 'polygon':
-        case 'polygon-mumbai':
-            for (const token of list.tokens) {
-                if (token.chainId !== chainId) {
-                    continue;
-                }
-                assets[token.address] = {
-                    address: token.address,
-                    name: token.name,
-                    symbol: token.symbol,
-                    decimals: token.decimals,
-                    logoURI: token.logoURI,
-                };
-            }
-            break;
-        case 'ethereum':
-        default:
-            if (list.tokens.findIndex(token => token.address === config.addresses.weth) !== -1) {
-                assets.ether = {
-                    address: 'ether',
-                    name: 'Ether',
-                    symbol: 'ETH',
-                    decimals: 18,
-                    logoURI: ETH_LOGO,
-                };
-            }
-            for (const token of list.tokens) {
-                if (token.chainId !== chainId) {
-                    continue;
-                }
-                assets[token.address] = {
-                    address: token.address,
-                    name: token.name,
-                    symbol: token.symbol,
-                    decimals: token.decimals,
-                    logoURI: token.logoURI,
-                };
-            }
-            break;
+            assets[token.address] = {
+                address: token.address,
+                name: token.name,
+                symbol: token.symbol,
+                decimals: token.decimals,
+                logoURI: token.logoURI,
+            };
+        }
+        break;
     }
     return assets;
 }
